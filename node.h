03e24507fe848e3911a8542a02f38404d9264c3b
Amit <amit.so100@gmail.com>
2017-03-14 17:45:24 +0200

    Added comments on node.h

--- node.h
+++ node.h
@@ -3,10 +3,20 @@
 
 typedef struct node_t* Node;
 
+//Creates a new node with integer value.
+//Returns NULL if failed.
 Node nodeCreate(int x);
+
+//Destroys given node.
 void nodeDestroy(Node n);
+
+//Returns value of the current node.
 int nodeValue(Node n);
+
+//Returns next node after given one.
 Node nodeGetNext(Node n);
+
+//Sets the node after the given one.
 void nodeSetNext(Node org, Node next);
 
 #endif
