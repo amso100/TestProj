ffe27b75b41879264829342bb93681b1ab384956
Amit <amit.so100@gmail.com>
2017-03-14 17:53:15 +0200

    Added nodes test setup, still need to add destroying

--- prog.c
+++ prog.c
@@ -6,7 +6,19 @@ int main() {
 	Node first = nodeCreate(1);
 	Node next1 = nodeCreate(2);
 	nodeSetNext(first, next1);
-	//Add code for checking rest of functions,
-	//and destroying nodes.
-	//Also add comments.
+	Node next2 = nodeCreate(3);
+	nodeSetNext(next1, next2);
+	Node next3 = nodeCreate(5);
+	nodeSetNext(next2, next3);
+	printf("Finished setting up nodes.\n");
+
+	printf("Printing nodes...\n");
+	Node iter = first;
+	while(iter != NULL) {
+		printf("Node value is %d\n", nodeValue(iter));
+		iter = nodeGetNext(iter);
+	}
+	printf("Finished printing nodes.");
+	//Add code for destroying nodes.
+	return 0;
 }
